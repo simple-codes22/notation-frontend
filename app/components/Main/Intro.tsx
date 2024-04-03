c1e7d2857e098a5a1361e4f7d7c7fa6b1a13b00b
Simeon Akanbi <simeonakanbi99@gmail.com>
2024-04-03 14:24:07 +0100

    Making a responsive intro

--- app/components/Main/Intro.tsx
+++ app/components/Main/Intro.tsx
@@ -4,8 +4,8 @@ import React from 'react'
 const Intro = () => {
   return (
     <Flex width='100%' justify='center'>
-        <Box width='100%' maxWidth='1280px' display='flex' minH='720px'>
-            <Box width='60%'>
+        <Box width='100%' maxWidth='1280px' display='flex' flexDirection={{base:'column', md: 'row'}} minH='720px'>
+            <Box width={{base:"100%", md:'60%'}}>
                 <Text>
                     Elevate Your Math Mastery: Dive Into Our Dynamic Quiz Universe
                 </Text>
@@ -13,7 +13,7 @@ const Intro = () => {
                     Empower your math journey with interactive quizzes! Embark on a thrilling adventure of learning where every question leads you to mastery.
                 </Text>
             </Box>
-            <Box></Box>
+            <Box width={{base: '100%', md:'40%'}}></Box>
         </Box>
     </Flex>
   )
