3614649c1a734e060efbd28437f059b7e3d367f1
Simeon Akanbi <simeonakanbi99@gmail.com>
2024-04-05 12:11:27 +0100

    Add image illustration for intro section

--- app/components/Main/Intro.tsx
+++ app/components/Main/Intro.tsx
@@ -1,10 +1,13 @@
 import { Flex, Box, Heading, Text } from '@chakra-ui/react'
-import React from 'react'
+import React from 'react';
+// import Background from '../../../assets/Images/SVG/Background-1.svg';
+// import Image from 'next/image';
 
 const Intro = () => {
   return (
     <Flex width='100%' justify='center'>
         <Box width='100%' maxWidth='1280px' display='flex' flexDirection={{base:'column', md: 'row'}} minH='720px'>
+            {/* <Image src={Background} alt='Background' layout='fill' objectFit='cover' objectPosition='center'/> */}
             <Box width={{base:"100%", md:'60%'}}>
                 <Text>
                     Elevate Your Math Mastery: Dive Into Our Dynamic Quiz Universe
