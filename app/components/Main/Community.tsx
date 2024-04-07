d4053078c8b1cb7e2a27b276b2d553535c07acc8
Simeon Akanbi <simeonakanbi99@gmail.com>
2024-04-07 17:54:27 +0100

    Update metadata to include icons

--- app/components/Main/Community.tsx
+++ app/components/Main/Community.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Community = () => {
   return (
     <Flex as='section' width='100%' justify='center' align='center'>
-        <Box>
+        <Box width='100%' maxW='1280px'>
             Community
         </Box>
     </Flex>
--- app/components/Main/How It works.tsx
+++ app/components/Main/How It works.tsx
@@ -6,7 +6,7 @@ const HowItWorks = () => {
   return (
     <Flex as="section" width='100%' justify='center' align='center'>
       <Box width='100%' maxW='1280px'>
-        Benefits
+        How It Works
       </Box>
     </Flex>
   )
