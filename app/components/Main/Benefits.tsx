16306aeecd1b624bcdc7aa5e4f470e5b0c33b97b
Simeon Akanbi <simeonakanbi99@gmail.com>
2024-04-07 17:33:08 +0100

    Adding new section

--- app/components/Main/Benefits.tsx
+++ app/components/Main/Benefits.tsx
@@ -1,8 +1,14 @@
+'use client';
+import { Box, Flex } from '@chakra-ui/react'
 import React from 'react'
 
 const Benefits = () => {
   return (
-    <div>Benefits</div>
+    <Flex width='100%' justify='center' align='center'>
+      <Box width='100%' maxW='1280px'>
+        Benefits
+      </Box>
+    </Flex>
   )
 }
 
