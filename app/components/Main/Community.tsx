ec236ed52dd65d0dba7f37df4a830c1ae194dde6
Simeon Akanbi <simeonakanbi99@gmail.com>
2024-04-07 17:42:07 +0100

    Update on new sections

---
+++ app/components/Main/Community.tsx
@@ -0,0 +1,14 @@
+import { Flex, Box } from '@chakra-ui/react'
+import React from 'react'
+
+const Community = () => {
+  return (
+    <Flex as='section' width='100%' justify='center' align='center'>
+        <Box>
+            Community
+        </Box>
+    </Flex>
+  )
+}
+
+export default Community
---
+++ app/components/Main/Testimonial.tsx
@@ -0,0 +1,14 @@
+import { Box, Flex } from '@chakra-ui/react'
+import React from 'react'
+
+const Testimonial = () => {
+  return (
+    <Flex as='section' width='100%'>
+        <Box width='100%' maxW='1280px'>
+            Testimonial
+        </Box>
+    </Flex>
+  )
+}
+
+export default Testimonial
