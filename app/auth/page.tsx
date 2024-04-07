233d24a561f0253f094e5e2de3bb100cea0b5b80
Simeon Akanbi <simeonakanbi99@gmail.com>
2024-04-07 18:07:53 +0100

    Update to auth page

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,12 +1,19 @@
 'use client';
-import { Box } from '@chakra-ui/react'
+import { Box, Flex } from '@chakra-ui/react'
+import Image from 'next/image';
 import React from 'react';
+import Illustration from './../../assets/Images/Illustrations/Auth Illustration.jpg';
 
 const Page = () => {
   return (
-    <Box>
-      Auth
-    </Box>
+    <Flex justify='center' align='center' width='100%'>
+      <Box width='50%' display='flex' justifyContent='center' alignItems='center' height='100%'>
+        <Image src={Illustration} alt="Authentication" />
+      </Box>
+      <Box width='50%' display='flex' justifyContent='center' alignItems='center'>
+        Page
+      </Box>
+    </Flex>
   )
 }
 
