5d11af9b198771fe20de4a40efe933108d628783
Simeon Akanbi <simeonakanbi99@gmail.com>
2024-04-08 10:05:12 +0100

    Updating footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,54 +1,68 @@
 'use client';
-import { Box, Text } from '@chakra-ui/react';
+import { Box, Flex, Text } from '@chakra-ui/react';
 import Image from 'next/image';
 import { Link } from '@chakra-ui/next-js';
 import React from 'react'
 import mainLogo from '@/assets/Images/PNG/Notation normal tp.png';
+import { FaXTwitter, FaDiscord, FaFacebook, FaGithub, FaLinkedin } from 'react-icons/fa6';
 
 const Footer = () => {
   return (
     <Box width='100%' py='50px' display='flex' justifyContent='center' alignItems='center'>
       <Box width='100%' maxW='1280px'>
-        <Box>
+        <Box display='flex' justifyContent='space-between' alignItems='center'>
+
           <Box>
             <Box width='200px'>
               <Image src={mainLogo} alt='logo' />
             </Box>
           </Box>
+
+          <Flex justify='space-between'>
+            <Box mx='35px'>
+              <Text pb='10px'>Quick Links</Text>
+              <Box display='flex' flexDirection='column'>
+                <Link href='' my='3px' fontSize='14px'>Home</Link>
+                <Link href='' my='3px' fontSize='14px'>About</Link>
+                <Link href='' my='3px' fontSize='14px'>Features</Link>
+                <Link href='' my='3px' fontSize='14px'>Track</Link>
+                <Link href='' my='3px' fontSize='14px'>Community</Link>
+                <Link href='' my='3px' fontSize='14px'>Testimonial</Link>
+              </Box>
+            </Box>
+            <Box mx='35px'>
+              <Text pb='10px'>Resources</Text>
+              <Box display='flex' flexDirection='column'>
+                <Link href='' my='3px' fontSize='14px'>Blog</Link>
+                <Link href='' my='3px' fontSize='14px'>FAQ</Link>
+                <Link href='' my='3px' fontSize='14px'>Support</Link>
+                <Link href='' my='3px' fontSize='14px'>Privacy Policy</Link>
+                <Link href='' my='3px' fontSize='14px'>Terms of Service</Link>
+              </Box>
+            </Box>
+            {/* <Box mx='35px'>
+              <Text pb='10px'>Connect</Text>
+              <Box display='flex' flexDirection='column'>
+                <Link href='' my='3px' fontSize='14px'>Twitter</Link>
+                <Link href='' my='3px' fontSize='14px'>Facebook</Link>
+                <Link href='' my='3px' fontSize='14px'>Instagram</Link>
+                <Link href='' my='3px' fontSize='14px'>LinkedIn</Link>
+              </Box>
+            </Box> */}
+          </Flex>
+        </Box>
+        <Box display='flex' justifyContent='space-between' pt='50px'>
           <Box>
-            <Box>
-              <Text>Quick Links</Text>
-              <Box>
-                <Link href=''>Home</Link>
-                <Link href=''>About</Link>
-                <Link href=''>Features</Link>
-                <Link href=''>Track</Link>
-                <Link href=''>Community</Link>
-                <Link href=''>Testimonial</Link>
-              </Box>
-            </Box>
-            <Box>
-              <Text>Resources</Text>
-              <Box>
-                <Link href=''>Blog</Link>
-                <Link href=''>FAQ</Link>
-                <Link href=''>Support</Link>
-                <Link href=''>Privacy Policy</Link>
-                <Link href=''>Terms of Service</Link>
-              </Box>
-            </Box>
-            <Box>
-              <Text>Connect</Text>
-              <Box>
-                <Link href=''>Twitter</Link>
-                <Link href=''>Facebook</Link>
-                <Link href=''>Instagram</Link>
-                <Link href=''>LinkedIn</Link>
-              </Box>
-            </Box>
+            &copy; {(new Date()).getUTCFullYear()} Notation. All rights reserved.
+          </Box>
+          <Box display='flex'>
+            <Link href="" mx='5px'><FaXTwitter size='28px' /></Link>
+            <Link href="" mx='5px'><FaLinkedin size='28px' /></Link>
+            <Link href="" mx='5px'><FaFacebook size='28px' /></Link>
+            <Link href="" mx='5px'><FaGithub size='28px' /></Link>
+            <Link href="" mx='5px'><FaDiscord size='28px' /></Link>
           </Box>
         </Box>
-        <Box></Box>
       </Box>
     </Box>
   )
