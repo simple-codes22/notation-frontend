6895594195377e0e69397e18b9ba8e07d4d4dde4
Simeon Akanbi <simeonakanbi99@gmail.com>
2024-04-08 01:25:21 +0100

    Change to the layout of the feature section

--- app/components/Main/Features.tsx
+++ app/components/Main/Features.tsx
@@ -4,75 +4,93 @@ import React from "react";
 
 const Features = () => {
   const mainFeatures = [
-    {
-      title: "Interactive Quizzes",
-      desc_:
-        "Engage in dynamic quizzes that make learning math enjoyable and effective. Our interactive platform offers a variety of question formats, ensuring an engaging experience for learners of all levels.",
-    },
-    {
-      title: "Customization Options",
-      desc_:
-        "Tailor your quiz experience to suit your learning preferences. Choose from a range of topics and difficulty levels, allowing you to focus on specific areas or challenge yourself with comprehensive assessments.",
-    },
-    {
-      title: "Real-Time Feedback",
-      desc_:
-        "Receive instant feedback on your quiz results, helping you identify strengths and areas for improvement. Our detailed analytics and insights empower you to track your progress and stay motivated on your math journey.",
-    },
-    {
-      title: "Comprehensive Question Bank",
-      desc_:
-        "Explore a vast library of math topics, from basic arithmetic to advanced calculus. Our extensive question bank ensures that you have access to a wide range of exercises to enhance your skills and knowledge.",
-    },
-    {
-      title: "Progress Tracking",
-      desc_:
-        "Monitor your progress over time and visualize your improvement with our intuitive progress tracking tools. Stay informed about your performance and celebrate your achievements as you work towards mastering math concepts.",
-    },
-    {
-      title: "Accessible Anywhere, Anytime",
-      desc_:
+    [
+      {
+        title: "Interactive Quizzes",
+        desc_:
+          "Engage in dynamic quizzes that make learning math enjoyable and effective. Our interactive platform offers a variety of question formats, ensuring an engaging experience for learners of all levels.",
+      },
+      {
+        title: "Customization Options",
+        desc_:
+          "Tailor your quiz experience to suit your learning preferences. Choose from a range of topics and difficulty levels, allowing you to focus on specific areas or challenge yourself with comprehensive assessments.",
+      },
+    ],
+    [
+      {
+        title: "Real-Time Feedback",
+        desc_:
+          "Receive instant feedback on your quiz results, helping you identify strengths and areas for improvement. Our detailed analytics and insights empower you to track your progress and stay motivated on your math journey.",
+      },
+    ],
+    [
+      {
+        title: "Comprehensive Question Bank",
+        desc_:
+          "Explore a vast library of math topics, from basic arithmetic to advanced calculus. Our extensive question bank ensures that you have access to a wide range of exercises to enhance your skills and knowledge.",
+      },
+      {
+        title: "Accessible Anywhere, Anytime",
+        desc_:
         "Access our exam suite from any device with an internet connection, allowing you to study math whenever and wherever it's convenient for you. Whether you're at home, in school, or on the go, our platform is accessible 24/7.",
-    },
-    {
-      title: "Engaging Learning Environment",
-      desc_:
-        "Immerse yourself in an interactive learning environment that fosters curiosity and exploration. Our platform is designed to make math learning fun and exciting, encouraging you to embrace challenges  and unlock your full potential.",
-    },
-    {
-      title: "Community Support",
-      desc_:
-        "Connect with a community of passionate learners and educators who share your love for math. Share tips, ask questions, and collaborate with others to enhance your learning experience and achieve your goals.",
-    },
+      },
+    ],
+    // {
+    //   title: "Progress Tracking",
+    //   desc_:
+    //     "Monitor your progress over time and visualize your improvement with our intuitive progress tracking tools. Stay informed about your performance and celebrate your achievements as you work towards mastering math concepts.",
+    // },
+    // {
+    //   title: "Engaging Learning Environment",
+    //   desc_:
+    //     "Immerse yourself in an interactive learning environment that fosters curiosity and exploration. Our platform is designed to make math learning fun and exciting, encouraging you to embrace challenges  and unlock your full potential.",
+    // },
   ];
 
   
   return (
-    <Flex width='100%' justify='center' >
+    <Flex width='100%' justify='center' flexDirection='column'>
         <Box as='section' width='100%' maxW='1280px'>
             <Text fontSize='50px' fontWeight={700} textAlign='center' mt='50px'>Our Amazing Features</Text>
+        </Box>
         <Box display='flex' flexDirection='column'>
-            {mainFeatures.map((elem, index) => {
-              if (index % 2 == 0) {
-                return (<Box key={index} width='60%' my='20px' p='40px'>
+          {mainFeatures.map((elem, index) => {
+            if (elem.length == 1) {
+              return (
+                <Box key={`${index}0`}>
+                  <Box width='100%' my='20px' p='40px'>
                     <Text py='20px' fontSize='30px' fontWeight='700'>
-                        {elem.title}
+                        {elem[0].title}
                     </Text>
                     <Text py='20px' fontSize='20px' letterSpacing='1px'>
-                        {elem.desc_}
+                        {elem[0].desc_}
                     </Text>
-                </Box>)
-              }
-                return (<Box key={index} width='60%' my='20px' p='40px' alignSelf='flex-end'>
-              <Text py='20px' fontSize='30px' fontWeight='700'>
-                  {elem.title}
-              </Text>
-              <Text py='20px' fontSize='20px' letterSpacing='1px'>
-                  {elem.desc_}
-              </Text>
-          </Box>)
-            })}
-        </Box>
+                  </Box>
+                </Box>
+              )
+            } else if(elem.length == 2) {
+              return (
+                <Box key={`${index}0`} display='flex'>
+                  <Box width='50%' my='20px' p='40px'>
+                    <Text py='20px' fontSize='30px' fontWeight='700'>
+                        {elem[0].title}
+                    </Text>
+                    <Text py='20px' fontSize='20px' letterSpacing='1px'>
+                        {elem[0].desc_}
+                    </Text>
+                  </Box>
+                  <Box width='50%' my='20px' p='40px'>
+                    <Text py='20px' fontSize='30px' fontWeight='700'>
+                        {elem[1].title}
+                    </Text>
+                    <Text py='20px' fontSize='20px' letterSpacing='1px'>
+                        {elem[1].desc_}
+                    </Text>
+                  </Box>
+                </Box>
+              )
+            }
+          })}
         </Box>
     </Flex>
   );
--- app/context/chakra-provider.tsx
+++ app/context/chakra-provider.tsx
@@ -1,7 +1,11 @@
 // app/providers.tsx
 'use client';
 
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+
+const theme = extendTheme({
+    
+})
 
 export function Providers({ children }: { children: React.ReactNode }) {
     return <ChakraProvider>{children}</ChakraProvider>
