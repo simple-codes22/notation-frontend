a9d41bee524b0205050f59f4624dbeeaf634eded
Simeon Akanbi <simeonakanbi99@gmail.com>
2024-04-08 10:18:37 +0100

    Change auth image to background

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -5,10 +5,14 @@ import React from 'react';
 import Illustration from './../../assets/Images/Illustrations/Auth Illustration.jpg';
 
 const Page = () => {
+  // console.log(Illustration)
   return (
     <Flex justify='center' align='center' width='100%'>
-      <Box width='50%' display='flex' justifyContent='center' alignItems='center' height='100%'>
-        <Image src={Illustration} alt="Authentication" />
+      <Box width='50%' display='flex' justifyContent='center' alignItems='center' minH='100vh' sx={{
+        background: `url('${Illustration.src}')`,
+        backgroundSize: 'contain',
+        backgroundPosition: 'center',
+      }}>
       </Box>
       <Box width='50%' display='flex' justifyContent='center' alignItems='center'>
         Page
