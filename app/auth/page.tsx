cfbe01d9ff22417a90ca75f925783c15fd45d83e
Simeon Akanbi <simeonakanbi99@gmail.com>
2024-04-11 19:35:35 +0100

    update

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,8 +1,9 @@
 'use client';
-import { Box, Flex, Input, Text } from '@chakra-ui/react'
+import { Box, Flex, Input, Text, InputGroup, InputLeftElement, Button, Divider, AbsoluteCenter } from '@chakra-ui/react'
 import Image from 'next/image';
 import React from 'react';
 import Illustration from './../../assets/Images/Illustrations/Auth Illustration.jpg';
+import { FaLock, FaUser } from 'react-icons/fa6';
 
 const Page = () => {
   // console.log(Illustration)
@@ -14,14 +15,38 @@ const Page = () => {
         backgroundPosition: 'center',
       }}>
       </Box>
-      <Box width='50%' display='flex' justifyContent='center' alignItems='center'>
-        <Box>
-          <Text>Sign Up</Text>
-          <Box>
-            <Input placeholder='email' />
-            <Input placeholder='username' />
-            <Input placeholder='password' />
+
+      <Box width='50%' display='flex' flexDirection='column' justifyContent='center' alignItems='center'>
+        <Box display='flex' width='100%' flexDir='column' justifyContent='center' alignItems='center'>
+          <Text fontSize='24px' fontWeight='600'>Welcome!</Text>
+          <Text letterSpacing='1.2px'>Sign In To Your Account</Text>
+          <Box width='50%'>
+
+          <InputGroup width='100%' display='flex' justifyContent='center' alignItems='center' my='20px'>
+            <InputLeftElement pointerEvents='none' display='flex' justifyContent='center' alignItems='center'>
+              <FaUser fill='#00000063' />
+            </InputLeftElement>
+            <Input type='text' placeholder='Username'  variant='flushed' />
+          </InputGroup>
+            
+          <InputGroup width='100%' display='flex' justifyContent='center' alignItems='center' my='20px'>
+              <InputLeftElement pointerEvents='none' display='flex' justifyContent='center' alignItems='center'>
+                <FaLock fill='#00000063' />
+              </InputLeftElement>
+              <Input placeholder='Password' type='password' variant='flushed' />
+            </InputGroup>
           </Box>
+        </Box>
+
+        <Box my='20px'>
+          <Button>Sign In</Button>
+        </Box>
+
+        <Box position='relative' padding='10'>
+          <Divider />
+          <AbsoluteCenter px='4'>
+            Content
+          </AbsoluteCenter>
         </Box>
       </Box>
     </Flex>
