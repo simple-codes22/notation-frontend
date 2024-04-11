039c5cea1379f413e57c0e57939b6bd8842c7dde
Simeon Akanbi <simeonakanbi99@gmail.com>
2024-04-11 12:17:25 +0100

    Update the font size of the titles

--- app/components/Main/Testimonial.tsx
+++ app/components/Main/Testimonial.tsx
@@ -5,7 +5,7 @@ const Testimonial = () => {
   return (
     <Flex as='section' width='100%' justify='center' align='center'>
         <Box width='100%' maxW='1280px'>
-            <Box><Text textAlign='center'>Testimonial</Text></Box>
+            <Box><Text textAlign='center' fontSize='50px' fontWeight={700} mt='50px'>Testimonial</Text></Box>
             <Box></Box>
         </Box>
     </Flex>
