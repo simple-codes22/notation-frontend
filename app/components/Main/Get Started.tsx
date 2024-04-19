792b9649f782452948e98d70fb4369c003d8dd65
Simeon Akanbi <simeonakanbi99@gmail.com>
2024-04-19 01:30:31 +0100

    Add new link

--- app/components/Main/Get Started.tsx
+++ app/components/Main/Get Started.tsx
@@ -1,6 +1,7 @@
 'use client';
 import React from 'react'
-import { Box, Button, Flex, Text } from '@chakra-ui/react'
+import { Box, Button, Flex, Text } from '@chakra-ui/react';
+import { Link } from '@chakra-ui/next-js';
 
 const GetStarted = () => {
   return (
@@ -10,7 +11,7 @@ const GetStarted = () => {
                 <Text fontSize='50px' textAlign='center'>
                     Ready to get started? Sign up now and take your first step towards mastering math!
                 </Text>
-                <Button my='20px' py='30px' px='30px' fontSize='20px'  variant='solid' background="#383838" color='white' _hover={{background: '#2d2d2de2'}}>Let&apos;s Go🚀🚀</Button>
+                <Button my='20px' as={Link} py='30px' px='30px' fontSize='20px' href='/auth' variant='solid' background="#383838" color='white' _hover={{background: '#2d2d2de2', textDecor: 'none'}}>Let&apos;s Go🚀🚀</Button>
             </Box>
         </Box>
     </Flex>
