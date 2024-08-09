cc8320a8de5053b6685357bd89b35625e259fca0
Simeon Akanbi <simeonakanbi99@gmail.com>
2024-08-09 15:10:29 +0100

    Status check

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -2,11 +2,32 @@
 import { Box, Button, Flex } from '@chakra-ui/react';
 import { Link } from '@chakra-ui/next-js';
 import Image from 'next/image';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Logo from  '@/assets/Images/PNG/Notation normal tp.png';
 import { FaBars } from 'react-icons/fa';
+import { NotationClient } from '../context/supabaseClientSet';
+import { UserContext } from '../context/UserContext';
+import { useContext } from 'react';
 
 const Header = () => {
+  const mainUser = useContext(UserContext)
+  const [logged, setLogged] = useState<Boolean>(false)
+  useEffect(() => {
+    console.log("Here")
+    const obtainUser = async () => {
+      if (mainUser?.user) {
+        setLogged(true)
+      } else {
+        const { data: { user } } = await NotationClient.auth.getUser()
+        if (user) {
+          console.log(user);
+          // mainUser?.setUser(user)
+          setLogged(true)
+        }
+      }
+    }
+    obtainUser();
+  }, [mainUser])
   return (
     <Flex py='20px' width='100%' justify='center' align='center'>
         <Box px='20px' width='100%' maxWidth='1280px' display='flex' alignItems='center' justifyContent='space-between'>
@@ -25,9 +46,14 @@ const Header = () => {
             </Box>
           </Box>
           <Box display={{base: 'none', md:'flex'}} alignItems='center'>
+            {logged ? 
+            <Link href="/dashboard">
+              Dashboard
+            </Link> : 
             <Box>
               <Link href="/auth"><Button variant='solid' background="#383838" color='white' _hover={{background: '#2d2d2de2'}}>Sign In</Button></Link>
             </Box>
+            }
           </Box>
         </Box>
     </Flex>
