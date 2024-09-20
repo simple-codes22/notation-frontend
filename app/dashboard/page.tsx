6638fe8dc1805a40e5a73bf2ea6e6acd03e30c23
Simeon Akanbi <simeonakanbi99@gmail.com>
2024-09-20 02:29:40 +0100

    Refactor dashboard page layout

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,8 +3,10 @@ import { Box } from '@chakra-ui/react'
 
 const Page = () => {
   return (
-    <Box>
-      Dashboard
+    <Box background="rgb(232, 236, 240)" minH='100vh' color="#2d2d2d" w='100%' display='flex' justifyContent='center'>
+      <Box w='100%' maxWidth='1280px' display='flex' flexDir='column' alignItems='center'>
+        Test text
+      </Box>
     </Box>
   )
 }
